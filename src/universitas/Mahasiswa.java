17ae5fd3e60cf4a9e105dfb96697cc93bdcb5725
muhammadrizkyuniska <muhammadrizkykeno@gmail.com>
2023-06-13 08:44:23 +0800

    Melangkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -8,5 +8,39 @@ public class Mahasiswa {
     public String getDetail(){
         return nama;
     }
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public Mahasiswa() {
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
     
 }
